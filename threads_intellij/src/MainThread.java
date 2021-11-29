b0a7eea094a3dd83fc31cd4338e1aecb3733afe7
Beltus Ambe <bchengwa@yahoo.com>
2021-11-29 16:53:45 -0500

    Delete threads_intellij directory

--- threads_intellij/src/MainThread.java
+++
@@ -1,9 +0,0 @@
-public class MainThread {
-    public static void main (String [] args)
-    {
-        System.out.println("This is the main thread");
-        SubThread subThread = new SubThread();
-        subThread.start();
-        System.out.println("This is the main thread again");
-    }
-}
--- threads_intellij/src/SubThread.java
+++
@@ -1,7 +0,0 @@
-public class SubThread extends Thread{
-    @Override
-    public void run()
-    {
-        System.out.println("This is the sub thread");
-    }
-}
